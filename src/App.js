e97db3f23021a30347e09a08cb2622c3e92450c6
Cristian CD <143566189+kriskoCD@users.noreply.github.com>
2024-10-03 19:35:50 -0500

    Update App.js

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Login from './components/Login';
+import Signup from './components/Signup';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
