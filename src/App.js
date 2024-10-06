50f598be2d49296210245155f6335b0639f04a99
FZuasnabar <139509842+FZuasnabar@users.noreply.github.com>
2024-10-06 00:33:00 -0500

    actu app js redireccion inicio import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // src/App.js
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Header from './components/Header';
 import Login from './components/Login';
 import Signup from './components/Signup';
