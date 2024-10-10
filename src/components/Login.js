a7d17a49805d43fc53bdae516d317091b846c52e
nikolas <nikolasdanteflorindezsanchez@gmail.com>
2024-10-10 12:35:51 -0500

    autenticacion 2 factores

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,29 +1,46 @@
 import React, { useState } from 'react';
 import './Login.css';
 import { useNavigate } from 'react-router-dom';
+import AuthService from '../funcionalidades/AuthService/AuthService'; // Importamos el AuthService para la lógica de autenticación
 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [is2FA, setIs2FA] = useState(false); // Manejamos si el 2FA está activo
+  const [verificationCode, setVerificationCode] = useState('');
+  const [generatedCode, setGeneratedCode] = useState('');
   const navigate = useNavigate();
 
+  // Manejamos el login usando el servicio de autenticación
   const handleLogin = (e) => {
     e.preventDefault();
 
-    const users = JSON.parse(localStorage.getItem('users')) || [];
-    const user = users.find(user => user.email === email && user.password === password);
-
+    const user = AuthService.login(email, password); // Usamos AuthService para la autenticación
     if (user) {
-      alert('Sesión iniciada');
-      localStorage.setItem('loggedInUser', email); // Guardar el usuario actual en localStorage
-      navigate('/inicio'); // Redirigir a la página de inicio
+      const code = AuthService.generate2FACode(); // Generamos el código de 2FA
+      setGeneratedCode(code);
+      setIs2FA(true); // Activamos el flujo de 2FA
+      alert(`Tu código de verificación es: ${code}`); // Simulamos el envío del código de verificación
     } else {
       alert('Credenciales incorrectas');
     }
   };
 
+  // Manejamos la verificación del código 2FA
+  const handle2FA = (e) => {
+    e.preventDefault();
+
+    if (AuthService.verify2FACode(verificationCode, generatedCode)) {
+      alert('Sesión iniciada correctamente');
+      localStorage.setItem('loggedInUser', email); // Guardar el usuario autenticado
+      navigate('/inicio'); // Redirigir a la página de inicio
+    } else {
+      alert('Código de verificación incorrecto');
+    }
+  };
+
   const handleForgotPassword = () => {
-    navigate('/restablecer-contraseña');  // Redirigir a la página de restablecer contraseña
+    navigate('/restablecer-contraseña'); // Redirigir a la página de restablecimiento de contraseña
   };
 
   return (
@@ -32,48 +49,63 @@ const Login = () => {
         <img src={process.env.PUBLIC_URL + '/corn-field.jpg'} alt="Corn Field" />
       </div>
       <div className="login-form">
-        <h2>Log In</h2>
-        <form onSubmit={handleLogin}>
-          <label htmlFor="email">Email Address</label>
-          <input
-            type="email"
-            id="email"
-            placeholder="Email Address"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
+        {!is2FA ? (
+          <form onSubmit={handleLogin}>
+            <h2>Log In</h2>
+            <label htmlFor="email">Email Address</label>
+            <input
+              type="email"
+              id="email"
+              placeholder="Email Address"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
 
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-
-          <div className="remember-forgot">
-            <div>
-              <input type="checkbox" id="remember" />
-              <label htmlFor="remember">Remember me</label>
+            <div className="remember-forgot">
+              <div>
+                <input type="checkbox" id="remember" />
+                <label htmlFor="remember">Remember me</label>
+              </div>
+              <a href="#" onClick={handleForgotPassword}>Forgot Password?</a>
             </div>
-            <a href="#" onClick={handleForgotPassword}>Forgot Password?</a>
-          </div>
 
-          <button type="submit">Log In</button>
+            <button type="submit">Log In</button>
 
-          <div className="social-login">
-            <button type="submit" className="google-login">Log in with Google</button>
-            <button type="submit" className="apple-login">Log in with Apple</button>
-          </div>
+            <div className="social-login">
+              <button type="submit" className="google-login">Log in with Google</button>
+              <button type="submit" className="apple-login">Log in with Apple</button>
+            </div>
 
-          <p>No account yet? <a href="/signup">Sign Up</a></p>
-        </form>
+            <p>No account yet? <a href="/signup">Sign Up</a></p>
+          </form>
+        ) : (
+          <form onSubmit={handle2FA}>
+            <h2 >Introduce el código de verificación</h2>
+            <label htmlFor="verificationCode">Código de Verificación</label>
+            <input
+              type="text"
+              id="verificationCode"
+              placeholder="6 dígitos"
+              value={verificationCode}
+              onChange={(e) => setVerificationCode(e.target.value)}
+              required
+            />
+            <button type="submit">Verificar</button>
+          </form>
+        )}
       </div>
     </div>
   );
-}
+};
 
 export default Login;
--- src/funcionalidades/AuthService/AuthService.js
+++ src/funcionalidades/AuthService/AuthService.js
@@ -1,16 +1,24 @@
 const AuthService = {
-    login: (email, password) => {
-      const users = JSON.parse(localStorage.getItem('users')) || [];
-      return users.find(user => user.email === email && user.password === password);
-    },
-    generate2FACode: () => {
-      return Math.floor(100000 + Math.random() * 900000).toString();
-    },
-    resetPassword: (email, newPassword) => {
-      const users = JSON.parse(localStorage.getItem('users')) || [];
-      const updatedUsers = users.map(user => user.email === email ? { ...user, password: newPassword } : user);
-      localStorage.setItem('users', JSON.stringify(updatedUsers));
-    }
-  };
-  export default AuthService;
+  login: (email, password) => {
+    const users = JSON.parse(localStorage.getItem('users')) || [];
+    return users.find(user => user.email === email && user.password === password);
+  },
+
+  generate2FACode: () => {
+    return Math.floor(100000 + Math.random() * 900000).toString();
+  },
+
+  verify2FACode: (enteredCode, generatedCode) => {
+    return enteredCode === generatedCode;
+  },
+
+  resetPassword: (email, newPassword) => {
+    const users = JSON.parse(localStorage.getItem('users')) || [];
+    const updatedUsers = users.map(user => user.email === email ? { ...user, password: newPassword } : user);
+    localStorage.setItem('users', JSON.stringify(updatedUsers));
+  }
+};
+
+export default AuthService;
+
   
